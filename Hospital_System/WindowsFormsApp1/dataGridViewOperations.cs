e4bc1d1e2c800d8a8bbcfcf28cc7c724aff033ea
nouewahed <Nourwahed1001@gmail.com>
2025-05-09 14:14:46 +0300

    del

--- Hospital_System/WindowsFormsApp1/dataGridViewOperations.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WindowsFormsApp1
-{
-    internal class dataGridViewOperations
-    {
-        public static object DataSource { get; internal set; }
-        public static object SelectedRows { get; internal set; }
-    }
-}
--- Hospital_System/WindowsFormsApp1/dateTimePicker1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WindowsFormsApp1
-{
-    internal class dateTimePicker1
-    {
-        public static object Value { get; internal set; }
-    }
-}
